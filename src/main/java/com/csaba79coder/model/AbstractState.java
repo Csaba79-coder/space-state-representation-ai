933726cf195744116c24a5dd900f55ea80894c84
webler-biology-physiology <csabavadasz79@gmail.com>
2023-11-28 12:54:19 +0100

    remove public access modifier as abstract methods are public by default (no needed public modifier!)

--- src/main/java/com/csaba79coder/model/AbstractState.java
+++ src/main/java/com/csaba79coder/model/AbstractState.java
@@ -5,11 +5,11 @@ package com.csaba79coder.model;
  */
 public abstract class AbstractState implements Cloneable {
     // Checks if the internal state is a state.
-    public abstract boolean isState();
+    abstract boolean isState();
     // Checks if the internal state is a goal state.
-    public abstract boolean isGoalState();
+    abstract boolean isGoalState();
     // Returns the number of operators available.
-    public abstract int getOperatorCount();
+    abstract int getOperatorCount();
     // Access all operators through the super operator.
     // Returns true if the i-th base operator can be applied to the internal state.
     // Call this method from a for loop, starting from 0 to the number of base operators.
@@ -20,7 +20,7 @@ public abstract class AbstractState implements Cloneable {
     //         System.out.println("The " + i + "th operator can be applied to the state " + state);
     //     }
     // }
-    public abstract boolean isSuperOperator(int i);
+    abstract boolean isSuperOperator(int i);
     // Clone method. This method is required because we may need to undo the effects of some operators.
     // The simplest way is to clone the state, apply the operator to the clone, and check for problems.
     // If there are no issues, the clone becomes the new state for further search.
